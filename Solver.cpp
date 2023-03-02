5c0da63995f663d5c4218d2fa120295dd7792b1d
0Xiaohei0 <15852786007ysc@gmail.com>
2023-03-02 14:45:31 -0500

    performance optimization

--- Solver.cpp
+++ Solver.cpp
@@ -54,12 +54,16 @@ public:
 	void solveCollisions()
 	{
 		const auto objectCount = objectList.size();
-		for (VerletObject& object_1 : objectList) {
-			for (VerletObject& object_2 : objectList) {
-				if (std::addressof(object_1) == std::addressof(object_2)) continue;
+		for (uint64_t i{ 0 }; i < objectCount; ++i) {
+			VerletObject& object_1 = objectList[i];
+			for (uint64_t j{ i + 1 }; j < objectCount; ++j) {
+				VerletObject& object_2 = objectList[j];
+				//if (std::addressof(object_1) == std::addressof(object_2)) continue;
 				const sf::Vector2f collision_axis = object_1.position - object_2.position;
-				const float dist = std::sqrt((collision_axis.x * collision_axis.x) + (collision_axis.y * collision_axis.y));
-				if (dist <= (object_1.radius + object_2.radius)) {
+				const float dist2 = (collision_axis.x * collision_axis.x) + (collision_axis.y * collision_axis.y);
+				const float min_dist = object_1.radius + object_2.radius;
+				if (dist2 < min_dist * min_dist) {
+					const float dist = sqrt(dist2);
 					const sf::Vector2f n = collision_axis / dist;
 					const float delta = (object_1.radius + object_2.radius) - dist;
 					object_1.position += 0.5f * delta * n;
--- particleImageDemo.cpp
+++ particleImageDemo.cpp
@@ -12,13 +12,13 @@ class ParticleImageDemo
 	const int WINDOW_SIZE_X = 1280;
 	const int WINDOW_SIZE_Y = 720;
 
-	const int frame_rate = 60;
+	const int frame_rate = 144;
 	const uint32_t     max_objects_count = 2100;
-	const float        object_spawn_delay = 0.002f;
+	const float        object_spawn_delay = 0.025f;
 	const float        snapshotDelay = 15.0f;
 	const float        displayDelay = 10.0f;
 	const float        object_spawn_speed = 1200.0f;
-	const sf::Vector2f object_spawn_position = { 500.0f, 200.0f };
+	const sf::Vector2f object_spawn_position = { 500.0f, 50.0f };
 	const float        object_min_radius = 3.0f;
 	const float        object_max_radius = 8.0f;
 	const float		   PI = 3.1415926535897932386f;
